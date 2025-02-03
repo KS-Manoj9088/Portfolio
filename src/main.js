822c42a41af83dfc436f72db07f3ce1bc99b742f
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-02-04 00:02:35 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './modern-normalise.css';
+import './style.css';
+import '../components/header.css';
+import '../components/hero.css';
+import '../components/about.css';
+import './utils.css';
+
+const btn = document.getElementsByClassName("btn");
+btn[0].addEventListener("click", function(){
+  alert("Hello, World!");
+});
+
