0578bee9b74f94da726550b246f579f933ef4b56
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-04-06 09:06:40 +0530

    Worked On PortFolio Project

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import './style.css';
 import '../components/header.css';
 import '../components/hero.css';
 import '../components/about.css';
+import '../components/featured.css';
+import '../components/work.css';
+import '../components/contact.css';
+import '../components/footer.css';
+import '../components/mobile-nav.css'
 import './utils.css';
 
-const btn = document.getElementsByClassName("btn");
-btn[0].addEventListener("click", function(){
-  alert("Hello, World!");
-});
+import mobileNav from './mobile-nav';
+import darkMode from './dark-mode';
+import lazyLoading from './lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
+
 
